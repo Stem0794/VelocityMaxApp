a8440e0aa0469cbed72fab228555d0580b4abfc8
Stem0794 <215477546+Stem0794@users.noreply.github.com>
2026-08-20 20:24:02 +0200

    style: load navigation UX layer

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './harbor.css'
+import './ux-shell.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
